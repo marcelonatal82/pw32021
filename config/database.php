927d232d09820190fcd157cf11b26bc67f3220fc
marcelonatal82 <alvesnatal@gmail.com>
2021-10-31 22:06:05 -0300

    Aula dia: 14/10/2021 atualizando

--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'movies_catalog'),
             'username' => env('DB_USERNAME', 'phpmyadmin'),
-            'password' => env('DB_PASSWORD', '123456'),
+            'password' => env('DB_PASSWORD', 'phpmyadmin'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
